3931ee70275a36e04a569a1e72daacf1bd7757bf
Julia <floc@unpromptedtirade.com>
2023-09-30 18:42:29 -0400

    Make this less silly

--- src/span.rs
+++ src/span.rs
@@ -28,10 +28,8 @@ impl Span {
 	pub fn get_line_num(&self, source: &str) -> usize {
 		let mut current_line_num = 1;
 
-		for (index, byte) in source.as_bytes().iter().enumerate() {
-			if index >= self.start {
-				break;
-			} else if matches!(byte, b'\n') {
+		for &byte in &source.as_bytes()[..self.start] {
+			if byte == b'\n' {
 				current_line_num += 1;
 			}
 		}
