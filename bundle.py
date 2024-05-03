6cee9f788e3e625e94fa8c17965713c39ab9584e
Julia <floc@unpromptedtirade.com>
2024-05-02 23:03:11 -0400

    Meta: Build LLVM statically & add bundle script

---
+++ bundle.py
@@ -0,0 +1,98 @@
+import os
+import shutil
+import subprocess
+import platform
+import argparse
+from datetime import datetime
+
+def linux_main(args):
+	print("Ensuring that Rust `x86_64-unknown-linux-musl` target toolchain is installed")
+	print("This requires that the Rust installation be managed with rustup")
+	print()
+
+	subprocess.run([
+		"rustup",
+		"target",
+		"add",
+		"x86_64-unknown-linux-musl",
+	])
+
+	print()
+	print("Building Fae compiler with musl in release mode")
+	print()
+
+	subprocess.run([
+		"cargo",
+		"build",
+		"--release",
+		"--target",
+		"x86_64-unknown-linux-musl",
+	])
+
+	print()
+	print("Copying files")
+
+	shutil.rmtree("./target/bundle", ignore_errors=True)
+	os.makedirs("./target/bundle/fae", exist_ok=True)
+
+	shutil.copy("./target/x86_64-unknown-linux-musl/release/fae", "./target/bundle/fae/fae")
+	shutil.copytree("./lib", "./target/bundle/fae/lib")
+
+	print("Archiving bundle")
+
+	os.chdir("./target")
+	shutil.make_archive("fae", "gztar", "./bundle")
+
+	print("Produced `./target/fae.tar.gz`")
+
+	if args.snapshot:
+		print()
+		print("Creating snapshot, this requires the GitHub cli be installed and signed in with approprate permissions")
+
+		if call_external(["git", "rev-parse", "--abbrev-ref", "HEAD"]) != b"main\n":
+			print("Safety check: Must be on main branch!")
+			return
+
+		if call_external(["git", "status", "--porcelain"]):
+			print("Safety check: Must be in a fully commited state!")
+			return
+
+		if call_external_stderr(["git", "push", "-n"]) != b"Everything up-to-date\n":
+			print("Safety check: Main must be fully pushed!")
+			return
+
+		snapshot_name = datetime.today().strftime("snapshot-%Y-%b-%d")
+		date_time = datetime.today().strftime("%A %B %-m %Y at %-I:%M")
+		print(f"Snapshot name: {snapshot_name}")
+
+		subprocess.run([
+			"gh",
+			"release",
+			"create",
+			snapshot_name,
+			"fae.tar.gz",
+			"--prerelease",
+			"--draft",
+			"--notes",
+			f"Fae programming language toolchain snapshot for Linux created {date_time}"
+		])
+
+def call_external(args):
+	process = subprocess.run(args, capture_output=True)
+	return process.stdout
+
+def call_external_stderr(args):
+	process = subprocess.run(args, capture_output=True)
+	return process.stderr
+
+parser = argparse.ArgumentParser(
+	prog="bundle.py",
+	description="Produces a redistributable bundle of the Fae programming language toolchain",
+)
+parser.add_argument("--snapshot", action="store_true")
+args = parser.parse_args()
+
+if platform.system().lower() == "linux":
+	linux_main(args)
+else:
+    print(f"Bundle script does not currently support {platform.system()}")
--- fetch_llvm.py
+++ fetch_llvm.py
@@ -5,9 +5,16 @@ import multiprocessing
 import platform
 
 def linux_main():
-	print("This script will fetch and build a local copy of LLVM for use by the Fae compiler")
-	print("System dependencies required for this step: an existing C++ toolchain, cmake, ninja, and git")
-	print("If any of these dependencies are missing then the fetch and build will fail")
+	print("This script will build a local copy of LLVM for use in developing and bundling the Fae compiler.")
+	print("To accomplish this it will clone LLVM, clone musl, build musl, build libc++ against musl, and")
+	print("finally build LLVM against both libc++ and musl. This process is a bit of a hack so it will be")
+	print("quite noisy and may take a while.")
+	print()
+	print("System dependencies required: a Clang+LLD toolchain, cmake, ninja, make, git, and a posix shell.")
+	print("If any of these dependencies are missing then the fetch and build will fail.")
+	print()
+	print("[press enter to continue]", end="")
+	input()
 	print()
 
 	if os.path.exists("./llvm"):
@@ -20,7 +27,7 @@ def linux_main():
 
 	cpu_count = multiprocessing.cpu_count()
 	jobs = user_number("How many compiler processes to use when building libc++ and LLVM?", cpu_count)
-	print("This script will require no further user input to continue; you can get a coffee now, this may take a while")
+	print("This script will require no further user input to continue; you can get a coffee now, this may take a while.")
 	print()
 
 	subprocess.run([
@@ -37,10 +44,55 @@ def linux_main():
 	])
 
 	print()
-	print("LLVM source files fetched, preparing to build libc++")
+	print("LLVM source files fetched, fetching musl")
 	print()
 
 	os.chdir("./llvm")
+	subprocess.run([
+		"git",
+		"-c",
+		"advice.detachedHead=false",
+		"clone",
+		"--depth",
+		"1",
+		"--branch",
+		"v1.2.3",
+		"git://git.musl-libc.org/musl",
+	])
+
+	print()
+	print("musl source files fetched, preparing to build musl")
+	print()
+
+	os.chdir("./musl")
+	subprocess.run(["./configure"], env={"CC": "clang"})
+
+	print()
+	print("musl build files written")
+	print()
+
+	subprocess.run(["make"], env={"CC": "clang"})
+
+	print()
+	print("musl has been built!")
+	print()
+
+	musl_include_dir = os.path.realpath("./include")
+	musl_obj_include_dir = os.path.realpath("./obj/include")
+	musl_arch_include_dir = os.path.realpath("./arch/x86_64")
+	musl_generic_include_dir = os.path.realpath("./arch/generic")
+	musl_lib_dir = os.path.realpath("./lib")
+	musl_crt1 = os.path.realpath("./lib/crt1.o")
+	musl_crti = os.path.realpath("./lib/crti.o")
+	musl_crtn = os.path.realpath("./lib/crtn.o")
+	musl_libc = os.path.realpath("./lib/libc.a")
+	musl_rt = os.path.realpath("./lib/librt.a")
+	musl_pthread = os.path.realpath("./lib/libpthread.a")
+	os.chdir("../")
+
+	print("Preparing to build libc++")
+	print()
+
 	os.chdir("libcxx")
 	os.makedirs("./build", exist_ok=True)
 	os.chdir("./build")
@@ -51,12 +103,16 @@ def linux_main():
 		"Ninja",
 		"-S",
 		"../../runtimes",
-		"-DLIBCXXABI_USE_LLVM_UNWINDER=OFF",
-		"-DLLVM_ENABLE_RUNTIMES=libcxx;libcxxabi",
+		"-DLIBUNWIND_ENABLE_SHARED=OFF",
+		"-DLIBCXX_ENABLE_SHARED=OFF",
+		"-DLIBCXXABI_USE_LLVM_UNWINDER=ON",
+		"-DLIBCXX_HAS_MUSL_LIBC=ON",
+		"-DLLVM_ENABLE_RUNTIMES=libcxx;libcxxabi;libunwind",
 		"-DCMAKE_C_COMPILER=clang",
 		"-DCMAKE_CXX_COMPILER=clang++",
-		"-DCMAKE_C_FLAGS=-Wno-everything",
-		"-DCMAKE_CXX_FLAGS=-Wno-everything",
+		f"-DCMAKE_C_FLAGS=-Wno-everything -Wno-undef -fPIC -I{musl_include_dir} -I{musl_obj_include_dir} -I{musl_arch_include_dir} -I{musl_generic_include_dir} -I/usr/include -nostdinc -nostdlib -nodefaultlibs",
+		f"-DCMAKE_CXX_FLAGS=-Wno-everything -Wno-undef -fPIC -I{musl_include_dir} -I{musl_obj_include_dir} -I{musl_arch_include_dir} -I{musl_generic_include_dir} -I/usr/include -nostdinc -nostdlib -nodefaultlibs",
+		f"-DCMAKE_EXE_LINKER_FLAGS={musl_crt1} {musl_crti} {musl_crtn} {musl_libc} {musl_rt}",
 		f"-DLLVM_PARALLEL_COMPILE_JOBS={jobs}",
 	])
 
@@ -71,7 +127,7 @@ def linux_main():
 	])
 
 	print()
-	print("libc++ has been and built!")
+	print("libc++ has been built!")
 	print("Preparing to build LLVM itself")
 	print()
 
@@ -81,6 +137,9 @@ def linux_main():
 
 	include_dir = os.path.realpath("../../libcxx/build/include/c++/v1")
 	lib_dir = os.path.realpath("../../libcxx/build/lib")
+	libcxx = os.path.realpath("../../libcxx/build/lib/libc++.a")
+	libcxx_abi = os.path.realpath("../../libcxx/build/lib/libc++abi.a")
+	libcxx_unwind = os.path.realpath("../../libcxx/build/lib/libunwind.a")
 
 	subprocess.run([
 		"cmake",
@@ -88,8 +147,12 @@ def linux_main():
 		"Ninja",
 		"-DCMAKE_C_COMPILER=clang",
 		"-DCMAKE_CXX_COMPILER=clang++",
-		"-DCMAKE_C_FLAGS=-Wno-everything",
-		f"-DCMAKE_CXX_FLAGS=-Wno-everything -Qunused-arguments -nostdinc++ -nostdlib++ -isystem {include_dir} -L {lib_dir} -Wl,-rpath,{lib_dir} -lc++",
+		f"-DCMAKE_C_FLAGS=-DHAVE_GETPAGESIZE -fPIC -nostdlib -nodefaultlibs -I{musl_include_dir} -I{musl_obj_include_dir} -I{musl_arch_include_dir} -I{musl_generic_include_dir}",
+		f"-DCMAKE_CXX_FLAGS=-DHAVE_GETPAGESIZE -fPIC -Qunused-arguments -nostdlib -nostdinc++ -nostdlib++ -I{include_dir} -I{musl_include_dir} -I{musl_obj_include_dir} -I{musl_arch_include_dir} -I{musl_generic_include_dir} -L {lib_dir} -L {musl_lib_dir} -Wl,-rpath,{lib_dir}",
+		f"-DCMAKE_EXE_LINKER_FLAGS={libcxx} {libcxx_abi} {libcxx_unwind} {musl_crt1} {musl_crti} {musl_crtn} {musl_libc} {musl_rt} {musl_pthread}",
+		f"-DCMAKE_MODULE_LINKER_FLAGS={libcxx} {libcxx_abi} {libcxx_unwind} {musl_libc} {musl_rt} {musl_pthread}",
+		f"-DCMAKE_SHARED_LINKER_FLAGS={libcxx} {libcxx_abi} {libcxx_unwind} {musl_libc} {musl_rt} {musl_pthread}",
+		f"-DCMAKE_STATIC_LINKER_FLAGS={libcxx} {libcxx_abi} {libcxx_unwind} {musl_libc} {musl_rt} {musl_pthread}",
 		"-DLLVM_ENABLE_LLD=ON",
 		"-DCMAKE_BUILD_TYPE=Release",
 		f"-DLLVM_PARALLEL_COMPILE_JOBS={jobs}",
@@ -99,6 +162,8 @@ def linux_main():
 		"-DLLVM_ENABLE_ZSTD=OFF",
 		"-DLLVM_ENABLE_TERMINFO=OFF",
 		"-DLLVM_ENABLE_LIBXML2=OFF",
+		"-DLLVM_BUILD_BENCHMARKS=OFF",
+		"-DLLVM_INCLUDE_BENCHMARKS=OFF",
 		"-DLLVM_TARGETS_TO_BUILD=X86",
 		".."
 	])
