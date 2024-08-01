1fbc0ddfd6f2b16e308099b1b5eeca24938b480e
Julia <floc@unpromptedtirade.com>
2024-08-01 14:08:25 -0400

    Meta: Codesign and notarize compiler on macOS

--- meta/bundle.py
+++ meta/bundle.py
@@ -41,6 +41,8 @@ def linux_main():
 	print("Copying files")
 
 	shutil.rmtree("./target/bundle", ignore_errors=True)
+	try: os.remove("./target/Fae.tar.gz")
+	except: pass
 	os.makedirs("./target/bundle/fae", exist_ok=True)
 
 	shutil.copy("./target/x86_64-unknown-linux-musl/release/fae", "./target/bundle/fae/fae")
@@ -49,11 +51,11 @@ def linux_main():
 	print("Archiving bundle")
 
 	os.chdir("./target")
-	shutil.make_archive("fae", "gztar", "./bundle")
+	shutil.make_archive("Fae", "gztar", "./bundle")
 
-	print("Produced `./target/fae.tar.gz`")
+	print("Produced `./target/Fae.tar.gz`")
 
-def macos_main():
+def macos_main(args):
 	print("Building Fae compiler in release mode")
 	print()
 
@@ -68,17 +70,62 @@ def macos_main():
 	print("Copying files")
 
 	shutil.rmtree("./target/bundle", ignore_errors=True)
+	try: os.remove("./target/Fae.dmg")
+	except: pass
 	os.makedirs("./target/bundle/fae", exist_ok=True)
 
 	shutil.copy("./target/release/fae", "./target/bundle/fae/fae")
 	shutil.copytree("./lib", "./target/bundle/fae/lib")
 
+	if args.identity_uuid is not None:
+		print("Signing executable")
+		subprocess.run([
+			"codesign",
+			"-s",
+			args.identity_uuid,
+			"-o",
+			"runtime",
+			"./target/bundle/fae/fae",
+		])
+
 	print("Archiving bundle")
 
 	os.chdir("./target")
-	shutil.make_archive("fae", "gztar", "./bundle")
+	# shutil.make_archive("fae", "zip", "./bundle")
+	subprocess.run([
+		"hdiutil",
+		"create",
+		"-volname",
+		"Fae",
+		"-srcfolder",
+		"./bundle/fae",
+		"-ov",
+		"-format",
+		"UDZO",
+		"Fae.dmg",
+	])
 
-	print("Produced `./target/fae.tar.gz`")
+	print("Produced `./target/Fae.dmg`")
+
+	if args.notarization_keychain_profile is not None:
+		print("Notarizing bundle")
+		subprocess.run([
+			"xcrun",
+			"notarytool",
+			"submit",
+			"./Fae.dmg",
+			"--keychain-profile",
+			args.notarization_keychain_profile,
+			"--wait",
+		])
+
+		print("Stapling bundle")
+		subprocess.run([
+			"xcrun",
+			"stapler",
+			"staple",
+			"./Fae.dmg",
+		])
 
 def call_external(args):
 	process = subprocess.run(args, capture_output=True)
@@ -88,14 +135,22 @@ def call_external_stderr(args):
 	process = subprocess.run(args, capture_output=True)
 	return process.stderr
 
+parser = argparse.ArgumentParser(
+	prog="bundle.py",
+	description="Produces a redistributable bundle of the Fae programming language toolchain",
+)
 platform = platform.system().lower()
 
 if os.path.basename(os.getcwd()) != "fae":
 	print("The current directory is not named \"fae\" which implies that we are not in the repo root.")
 	print("This script *MUST* be run from the repo root as it will fail otherwise, exiting.")
 elif platform == "linux":
+	args = parser.parse_args()
 	linux_main()
 elif platform == "darwin":
-	macos_main()
+	parser.add_argument("--identity-uuid", action="store", default=None)
+	parser.add_argument("--notarization-keychain-profile", action="store", default=None)
+	args = parser.parse_args()
+	macos_main(args)
 else:
     print(f"Bundle script does not currently support {platform.system()}")
